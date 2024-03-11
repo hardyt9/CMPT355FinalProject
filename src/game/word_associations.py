4f85b0b00f458ec0b2352ab5f541091e94f44d48
Malcolm Nielsen <malcolmgjpnielsen@hotmail.ca>
2024-03-10 23:02:16 -0600

    Implement function to calculate associations between supplied noun and specified adjectives

--- src/game/word_associations.py
+++ src/game/word_associations.py
@@ -6,6 +6,11 @@ def read_words_from_file(file_path):
         words = file.read().splitlines()
     return words
 
+'''
+calculate associations between given adjective and specified nouns.
+If which not supplied, defaults to all possible nouns.
+Which should be a list of strings if supplied.
+'''
 def get_adj_associations(adjective, which='all'):
 
      #Load English tokenizer, tagger, parser, and word vectors
@@ -24,6 +29,34 @@ def get_adj_associations(adjective, which='all'):
         nouns = which
 
     for noun in nouns:
+            similarity = nlp(adjective).similarity(nlp(noun))
+            associations[adjective][noun] = similarity
+
+    return associations
+
+'''
+calculate associations between given noun and specified adjectives.
+If which not supplied, defaults to all possible adjectives.
+Which should be a list of strings if supplied.
+'''
+def get_noun_associations(noun, which='all'):
+
+     #Load English tokenizer, tagger, parser, and word vectors
+    nlp = spacy.load("en_core_web_md")
+    #Create a defaultdict to store associations
+    associations = defaultdict(dict)
+
+    #Get associations of all nouns with the supplied adjective
+    if which == 'all':
+        #Read all adjectives from file
+        adjectives = read_words_from_file('../green_deck.txt')
+
+
+    #Get associations of specified adjectives with the supplied adjectives
+    else:
+        adjectives = which
+
+    for adjective in adjectives:
             similarity = nlp(adjective).similarity(nlp(noun))
             associations[adjective][noun] = similarity
 
