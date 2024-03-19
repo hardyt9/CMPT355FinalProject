b04007d8c7a8380e75d8d522aea66a43f9e52af4
Malcolm Nielsen <87519451+notMarth@users.noreply.github.com>
2024-03-18 18:25:00 -0600

    Change player class to act as template with abstract methods

--- src/game/player.py
+++ src/game/player.py
@@ -1,41 +1,15 @@
-from difflib import SequenceMatcher
+from abc import ABC, abstractmethod
 
+class Player(ABC):
 
-# create a player class for Apples to Apples which has a name and a hand of red cards and a score checks for AI players and associates correlation
-class Player:
-    def __init__(self, name, is_ai=False):
-        self.name = name        #name of the player
-        self.hand = []          #list of red cards in the player's hand
-        self.score = 0          #score of the player
-        self.is_ai = is_ai      #check if the player is an AI
-        self.correlation = {}   #correlation of the player's hand to the green card
+    @abstractmethod
+    def draw_card(self):
+        pass
 
-    def add_card_to_hand(self, card):
-        self.hand.append(card)
+    @abstractmethod
+    def play_card(self):
+        pass
 
-    def associate_correlation(self, green_card, red_cards):
-        if self.is_ai:
-            if green_card not in self.correlation:
-                self.correlation[green_card] = {'Perfect Match': [], 'High Match': [], 'Low Match': []}
-            for card in red_cards:
-                similarity = self.calculate_similarity(card, green_card)
-                if similarity == 1:
-                    self.correlation[green_card]['Perfect Match'].append(card)
-                elif similarity >= 0.7:
-                    self.correlation[green_card]['High Match'].append(card)
-                elif similarity >= 0.5:
-                    self.correlation[green_card]['Low Match'].append(card)
-
-    def play_red_card(self, green_card):
-        if self.is_ai:
-            best_match = None
-            best_similarity = 0
-            for card in self.hand:
-                similarity = self.calculate_similarity(card, green_card)
-                if similarity > best_similarity:
-                    best_similarity = similarity
-                    best_match = card
-            return best_match
-
-    def calculate_similarity(self, card1, card2):
-        return SequenceMatcher(None, card1, card2).ratio()
+    @abstractmethod
+    def pick_card(self):
+        pass
